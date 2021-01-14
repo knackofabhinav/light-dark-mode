435008773ab9e573bcea5e8216b9bd184e7b4201
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-01-14 06:11:06 +0530

    localStorage to save data

--- script.js
+++ script.js
@@ -39,9 +39,11 @@ function lightMode() {
 function switchTheme(event) {
     if(event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark')
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light')
         lightMode();
     }
 }
@@ -49,3 +51,13 @@ function switchTheme(event) {
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for themes
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
