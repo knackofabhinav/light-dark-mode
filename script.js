cd668003fd93febdbb065abcfe5609bbba08e456
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-01-14 06:32:38 +0530

    code cleanup

--- script.js
+++ script.js
@@ -13,38 +13,26 @@ function imageMode(color) {
     image3.src = `img/undraw_conceptual_idea_${color}.svg`; 
 }
 
-// Dark Mode Styles
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
+function toggleDarkLightMode(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon'):
+        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode('dark') : imageMode('light');
 }
 
-
-// Light Mode Styles
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    image1.src = 'img/undraw_proud_coder_light.svg';
-    image2.src = 'img/undraw_feeling_proud_light.svg';
-    image3.src = 'img/undraw_conceptual_idea_light.svg';
-    imageMode('light');
-}
 
 // Switch Theme Dynamically
 function switchTheme(event) {
     if(event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark')
-        darkMode();
+        toggleDarkLightMode(true);
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light')
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 
